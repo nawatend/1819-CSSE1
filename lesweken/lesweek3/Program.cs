161355749ff997c64064387c8aa4d2dcd3f3879c
nawatend <32384185+nawatend@users.noreply.github.com>
2018-10-10 12:01:15 +0200

    functie added

---
+++ lesweken/lesweek3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace lesweek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // het afdrukke nvan tekst in console.
+            Console.WriteLine("Hello World!");
+
+            // functiono maken  _ Rekenmachine
+
+
+            int a = 5;
+            int b = 6;
+            int resultaat1 = optellen(a, b);
+            int resultaat2 = deling(a, b);
+
+            Console.WriteLine(resultaat1);
+        }
+
+        static int optellen(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int deling(int a, int b)
+        {
+
+            return a / b;
+        }
+
+    }
+}
